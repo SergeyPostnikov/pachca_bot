a55ddb168a41b63846fcd3dce7cc85d048604c82
Sergey Postnikov <ser.post2017@gmail.com>
2024-12-06 15:53:03 +0300

    small update

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ async def webhook(event: ButtonEvent):
     return {"status": "success"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
